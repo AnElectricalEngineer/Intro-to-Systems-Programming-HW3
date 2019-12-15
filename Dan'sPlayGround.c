bf74e1f2b410bcb79ea6c6bf01848fd349c9af92
BIueMan <55624377+BIueMan@users.noreply.github.com>
2019-12-15 15:09:07 +0200

    Play Ground to test the function.

---
+++ Dan'sPlayGround.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "hash.h"
+//*****************************************************
+// tester function:
+//*****************************************************
+#define HASH_SIZE 2000
+
+int remainerOfHashSize(pKey key, int size)
+{
+	return ((int)key % size);
+}
+
+
+int main()
+{
+
+	return 0;
+}
