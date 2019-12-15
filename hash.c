ccc01ae6156fdb1a246be1ded07b3b4184df5530
BIueMan <55624377+BIueMan@users.noreply.github.com>
2019-12-15 15:06:58 +0200

    add HashCreate, not finish
    i think the hashTable shuld also include the address for the func. so i made a (size+1) array, that the first [size] address saved for the table.
    and the last for the func address.

--- hash.c
+++ hash.c
@@ -2,4 +2,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Insert your code here...
+// the [0 to (size-1)] are pointers in the array.
+// the (size) location is the address to the hash func 
+pHash HashCreate(unsigned int size, HashFunc func)
+{
+	pHash hashTable = (pHash)calloc(NULL, sizeof(int)*(size + 1));
+	if (hashTable == NULL)
+	{
+		printf("Error Allocating Memory");
+		exit(-1);
+		// need to add "Exit" from the program
+	}
+
+	hashTable + sizeof(int) * (size) = func; // still need to finish it, add the func to the last address of the array.
+
+	return hashTable;
+}
